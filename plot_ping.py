2127ef3466baccbf4805866dd7101c225204eafa
Sajjad Mirza <sajjad.mirza@mail.utoronto.ca>
2020-11-29 02:28:50 -0500

    update plot_ping to include download

--- plot_ping.py
+++ plot_ping.py
@@ -24,7 +24,23 @@ parser.add_argument('--out', '-o',
                     help="Output png file for the plot.",
                     default=None) # Will show the plot
 
+parser.add_argument('--download', '-d',
+                    help="Parse for download data.",
+                    default=None)
+
 args = parser.parse_args()
+
+def parse_down(fname):
+    ret = []
+    lines = open(fname).readlines()
+    num = 0
+    for line in lines:
+        try: 
+            ret.append([num, float(line)])
+            num += 1
+        except:
+            break
+    return ret
 
 def parse_ping(fname):
     ret = []
@@ -47,20 +63,27 @@ m.rc('figure', figsize=(16, 6))
 fig = figure()
 ax = fig.add_subplot(111)
 for i, f in enumerate(args.files):
-    data = parse_ping(f)
+    if args.download:
+        data = parse_down(f)
+    else:
+        data = parse_ping(f)
     if len(data) == 0:
         print >>sys.stderr, "%s: error: no ping data"%(sys.argv[0])
         sys.exit(1)
-
-    xaxis = map(float, col(0, data))
-    start_time = xaxis[0]
-    xaxis = map(lambda x: (x - start_time) / args.freq, xaxis)
+    if args.download:
+        xaxis = xaxis = map(float, col(0, data))
+    else: 
+        xaxis = map(float, col(0, data))
+        start_time = xaxis[0]
+        xaxis = map(lambda x: (x - start_time) / args.freq, xaxis)
     qlens = map(float, col(1, data))
-
     ax.scatter(xaxis, qlens, lw=2)
     ax.xaxis.set_major_locator(MaxNLocator(4))
 
-plt.ylabel("RTT (ms)")
+if args.download:
+    plt.ylabel("Download Time (s)")
+else:    
+    plt.ylabel("RTT (ms)")
 plt.grid(True)
 
 if args.out:
